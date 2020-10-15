749e6474fcd94f90cd7ad69eb0ce3704f2e727b9
Ракова Анна <arakova@ibs.ru>
2020-10-15 14:11:05 +0500

    Добавить цвет фона и корректировать запросы

--- src/api.js
+++ src/api.js
@@ -2,12 +2,12 @@ import {API_KEY, lang, units} from "./utils/local";
 
 export const api = {
     loadNextDaysWeather: (city) => {
-        return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${units}&lang=${lang}`)
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${units}&lang=${lang}`)
             .then((data) => data.json())
     },
 
     loadNowWeather: (city) => {
-        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}&lang=${lang}`)
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}&lang=${lang}`)
             .then((data) => data.json())
     }
 }
