7d7aad872ff4aa45eb3ddbcad2dee7f45f273611
Ракова Анна <arakova@ibs.ru>
2020-10-15 13:16:05 +0500

    Добавить папку docs

---
+++ docs/precache-manifest.b4c0447729869a8a86b56c4a28e686b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d66078bd58b94d239f4b7db1564285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a656b696435159a7b28e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b48752530fdc6ba12afd",
+    "url": "/static/css/main.6e3149ea.chunk.css"
+  },
+  {
+    "revision": "a656b696435159a7b28e",
+    "url": "/static/js/2.542d59df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.542d59df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48752530fdc6ba12afd",
+    "url": "/static/js/main.e345bc94.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "530e238f7185aa09c21e9581c847b346",
+    "url": "/static/media/home0.530e238f.jpg"
+  }
+]);
